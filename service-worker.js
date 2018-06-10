a99d7bbe5bc81a90f9089c09de2f80890d080de2
anywhere3D <anywhere3d@gmail.com>
2018-06-10 13:07:24 +0200

    Update service-worker.js

--- service-worker.js
+++ service-worker.js
@@ -1,1 +1,23 @@
 
+var debugMode = true;
+var CACHE_VERSION = "0.0.1";
+
+var jsFolder = "/js/";
+var cssFolder = "/css/";
+var scriptsFolder = "/scripts/";
+var componentsFolder = "/components/";
+var imgurFolder = "https://i.imgur.com/";
+
+var CURRENT_CACHES = {
+    jsCache : "cache-js-v"    + CACHE_VERSION,
+    cssCache: "cache-css-v"   + CACHE_VERSION,
+    imgCache: "cache-img-v"   + CACHE_VERSION,
+    scriptsCache: "scripts-v" + CACHE_VERSION,
+    componentsCache: "components-v" + CACHE_VERSION,
+};
+
+
+
+
+
+
